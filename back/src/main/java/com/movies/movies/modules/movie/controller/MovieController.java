28eeced202aee938a53b5c836210943bccca26d7
Edwin <20213tn046@utez.edu.mx>
2024-03-08 13:08:43 -0600

    [FIX] 400 bad reques with http status

--- back/src/main/java/com/movies/movies/modules/movie/controller/MovieController.java
+++ back/src/main/java/com/movies/movies/modules/movie/controller/MovieController.java
@@ -12,6 +12,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -28,72 +29,72 @@ public class MovieController {
     ObjectMapper objectMapper;
 
     @GetMapping("/page/")
-    ResponseApi<?> getPage(Pageable pageable){
+    ResponseEntity<ResponseApi<?>> getPage(Pageable pageable){
         try{
             Page<Movie> page = service.moviePage(pageable);
             // Convertir la página de películas a formato JSON y cifrar la cadena resultante
             String crypt = cryptService.encrypt(objectMapper.writeValueAsString(page));
             // Devolver una respuesta exitosa con la cadena cifrada
-            return new ResponseApi<>(crypt, HttpStatus.OK);
+            return new ResponseEntity<>(new ResponseApi<>(crypt, HttpStatus.OK), HttpStatus.OK);
         }catch (Exception e){
             // En caso de error, imprimir el mensaje de la excepción en la consola
             System.out.println(e.getMessage());
-            return new ResponseApi<>(null, HttpStatus.INTERNAL_SERVER_ERROR, true, "internal server error");
+            return new ResponseEntity<>(new ResponseApi<>(null, HttpStatus.INTERNAL_SERVER_ERROR, true, "internal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
     }
 
     @GetMapping("/page/name/{name}")
-    ResponseApi<?> getPageByName(@PathVariable(name = "name") String nameC, Pageable pageable){
+    ResponseEntity <ResponseApi<?>> getPageByName(@PathVariable(name = "name") String nameC, Pageable pageable){
         try{
             String name = cryptService.decrypt(nameC);
             Page<Movie> page = service.moviePageByName(name, pageable);
             String crypt = cryptService.encrypt(objectMapper.writeValueAsString(page));
-            return new ResponseApi<>(crypt, HttpStatus.OK);
+            return new ResponseEntity<>( new ResponseApi<>(crypt, HttpStatus.OK), HttpStatus.OK);
         }catch (Exception e){
             System.out.println(e.getMessage());
-            return new ResponseApi<>(null, HttpStatus.INTERNAL_SERVER_ERROR, true, "internal server error");
+            return new ResponseEntity<>( new ResponseApi<>(null, HttpStatus.INTERNAL_SERVER_ERROR, true, "internal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/page/director/{name}")
-    ResponseApi<?> getPageByDirector(@PathVariable(name = "name") String nameC, Pageable pageable){
+    ResponseEntity <ResponseApi<?>> getPageByDirector(@PathVariable(name = "name") String nameC, Pageable pageable){
         try{
             String name = cryptService.decrypt(nameC);
             Page<Movie> page = service.moviePageByDirector(name, pageable);
             String crypt = cryptService.encrypt(objectMapper.writeValueAsString(page));
-            return new ResponseApi<>(crypt, HttpStatus.OK);
+            return new ResponseEntity<>( new ResponseApi<>(crypt, HttpStatus.OK), HttpStatus.OK);
         }catch (Exception e){
             System.out.println(e.getMessage());
-            return new ResponseApi<>(null, HttpStatus.INTERNAL_SERVER_ERROR, true, "internal server error");
+            return new ResponseEntity<>( new ResponseApi<>(null, HttpStatus.INTERNAL_SERVER_ERROR, true, "internal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/page/gender/{name}")
-    ResponseApi<?> getPageByGender(@PathVariable(name = "name") String nameC, Pageable pageable){
+    ResponseEntity <ResponseApi<?>> getPageByGender(@PathVariable(name = "name") String nameC, Pageable pageable){
         try{
             String name = cryptService.decrypt(nameC);
             Page<Movie> page = service.moviePageByGender(name, pageable);
             String crypt = cryptService.encrypt(objectMapper.writeValueAsString(page));
-            return new ResponseApi<>(crypt, HttpStatus.OK);
+            return new ResponseEntity<>( new ResponseApi<>(crypt, HttpStatus.OK), HttpStatus.OK);
         }catch (Exception e){
             System.out.println(e.getMessage());
-            return new ResponseApi<>(null, HttpStatus.INTERNAL_SERVER_ERROR, true, "internal server error");
+            return new ResponseEntity<>( new ResponseApi<>(null, HttpStatus.INTERNAL_SERVER_ERROR, true, "internal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/page/betweenDates")
-    ResponseApi<Page<Movie>> getPageBetweenTwoDates(@Valid @RequestBody DtoDates dates, Pageable pageable){
-        return new ResponseApi<>( service.moviePageBetweenTwoDates(dates.getStart(), dates.getEnd(), pageable), HttpStatus.OK);
+    ResponseEntity <ResponseApi<Page<Movie>>> getPageBetweenTwoDates(@Valid @RequestBody DtoDates dates, Pageable pageable){
+        return new ResponseEntity<>( new ResponseApi<>( service.moviePageBetweenTwoDates(dates.getStart(), dates.getEnd(), pageable), HttpStatus.OK), HttpStatus.OK);
     }
 
     @PostMapping("/page/publication")
-    ResponseApi<Page<Movie>> getPageByPublication(@Valid @RequestBody DtoDates dates, Pageable pageable){
-        return new ResponseApi<>( service.moviePageByPublication(dates.getStart(), pageable), HttpStatus.OK);
+    ResponseEntity <ResponseApi<Page<Movie>>> getPageByPublication(@Valid @RequestBody DtoDates dates, Pageable pageable){
+        return new ResponseEntity<>( new ResponseApi<>( service.moviePageByPublication(dates.getStart(), pageable), HttpStatus.OK), HttpStatus.OK);
     }
 
     @PostMapping("/")
-    ResponseApi<?> insertMovie(@RequestBody String data) {
+    ResponseEntity <ResponseApi<?>> insertMovie(@RequestBody String data) {
         try {
             // Desencripta la cadena de datos recibida
             String movieString = cryptService.decrypt(data);
@@ -109,16 +110,16 @@ public class MovieController {
             if (!violations.isEmpty()) {
                 Map<String, String> validationErrors = new HashMap<>();
                 violations.forEach(violation -> validationErrors.put(violation.getPropertyPath() + "", violation.getMessage()));
-                return new ResponseApi<>(null, HttpStatus.BAD_REQUEST, true, "bad request", validationErrors);
+                return new ResponseEntity<>( new ResponseApi<>(null, HttpStatus.BAD_REQUEST, true, "bad request", validationErrors), HttpStatus.BAD_REQUEST);
             }
             // Si la validación es exitosa, inserta la película utilizando el servicio y devuelve una respuesta exitosa
-            return new ResponseApi<>(service.insertMovie(movie), HttpStatus.OK);
+            return new ResponseEntity<>( new ResponseApi<>(service.insertMovie(movie), HttpStatus.OK), HttpStatus.OK);
         }catch (JsonProcessingException ex){
             // Maneja la excepción en caso de que haya un problema al procesar el JSON recibido
-            return new ResponseApi<>(null, HttpStatus.BAD_REQUEST, true, "malformed request");
+            return new ResponseEntity<>( new ResponseApi<>(null, HttpStatus.BAD_REQUEST, true, "malformed request"), HttpStatus.BAD_REQUEST);
         } catch (Exception ex) {
             // Maneja la excepción en caso de un error general, por ejemplo, fallo en el servicio de cifrado
-            return new ResponseApi<>(null, HttpStatus.BAD_REQUEST, true, "failed crypt service");
+            return new ResponseEntity<>( new ResponseApi<>(null, HttpStatus.INTERNAL_SERVER_ERROR, true, "failed crypt service"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
